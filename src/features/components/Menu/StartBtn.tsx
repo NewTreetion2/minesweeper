c6a310bb7e483d7f9a526af9d78db8487da8914d
NewTreetion2 <115389778+NewTreetion2@users.noreply.github.com>
2023-06-25 03:49:17 +0900

    css 및 휴먼 에러 처리

--- src/features/components/Menu/StartBtn.tsx
+++ src/features/components/Menu/StartBtn.tsx
@@ -9,7 +9,7 @@ import { buttonInfo } from "store/types";
 function StartBtn() {
   const level = useSelector((state: RootState) => state.level.value);
   const { width, height, bomb } = level;
-  const cell = { state: 0, clicked: false, x: 0, y: 0 };
+  const cell = { state: 0, clicked: false, x: 0, y: 0, flag: false };
   const dispatch = useDispatch();
   //   const readyTable: number[][] = Array.from(Array(level.height), () =>
   //     Array(level.width).fill(0)
@@ -30,7 +30,7 @@ function StartBtn() {
       `가로: ${level.width} 세로: ${level.height} 폭탄: ${level.bomb}으로 게임을 시작합니다`
     );
     dispatch(gameReady());
-    dispatch(initTable(readyTable));
+    dispatch(initTable([readyTable, width, height, bomb]));
   };
 
   return <Button onClick={onClickHandler}>Game Start!</Button>;
--- src/features/components/Menu/index.ts
+++ src/features/components/Menu/index.ts
@@ -3,3 +3,4 @@ export { default as IntermediateBtn } from "components/Menu/IntermediateBtn";
 export { default as ExpertBtn } from "components/Menu/ExpertBtn";
 export { default as CustomBtn } from "components/Menu/CustomBtn";
 export { default as StartBtn } from "components/Menu/StartBtn";
+export { default as Menubar } from "components/Menu/Menubar";
